6aeefda63ba3259c00cb41ee978f53e09bd4e6b4
Jivites Damodar <djivites@gmail.com>
2026-05-16 16:10:59 +0530

    Update print statement in chatbot node initialization

--- src/langgraphagenticai/graph/graph_builder.py
+++ src/langgraphagenticai/graph/graph_builder.py
@@ -19,7 +19,7 @@ class GraphBuilder:
         and integrates it into the graph. The chatbot node is set as both the 
         entry and exit point of the graph.
         """
-        print("hello hi")
+        print("hello hi ji")
 
         self.basic_chatbot_node=BasicChatbotNode(self.llm)
 
